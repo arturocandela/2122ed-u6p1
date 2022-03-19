6f5c0701b9489d6c88245f9eb92d7676f2b8bb29
Sergio PM <spayam82@gmail.com>
2022-03-19 10:27:27 +0100

    [MOD] Refactor A3 extract constants

--- src/main/java/ed/sergi/Institut.java
+++ src/main/java/ed/sergi/Institut.java
@@ -2,14 +2,19 @@ package ed.sergi;
 
 public class Institut {
 
+	private static final int ANY = 2021;
+	private static final String CICLE_FORMATIU = "1º DAW";
+	private static final String CONGNOMS_ALUMNE = "Payà";
+	private static final String NOM_ALUMNE = "Sergi";
+
 	public static void main(String[] args) {
 		
 		Alumne alumneSergi = new Alumne();
 		
-		alumneSergi.nom = "Sergi";
-		alumneSergi.congNoms = "Payà";
-		alumneSergi.cicleFormatiu = "1º DAW";
-		alumneSergi.any = 2021;
+		alumneSergi.nom = NOM_ALUMNE;
+		alumneSergi.congNoms = CONGNOMS_ALUMNE;
+		alumneSergi.cicleFormatiu = CICLE_FORMATIU;
+		alumneSergi.any = ANY;
 		
 		System.out.println(alumneSergi.toString());
 
