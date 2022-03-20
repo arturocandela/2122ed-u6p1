ea0a30bf125f031d2484c075e1743d141c780bdf
ismaelmdv <ismaelmarcos64@gmail.com>
2022-03-20 22:20:00 +0100

    Refactorizar variables a constantes

--- src/main/java/ed/ismael/Institut.java
+++ src/main/java/ed/ismael/Institut.java
@@ -2,14 +2,19 @@ package ed.ismael;
 
 public class Institut {
 
+	private static final int ANY = 2021;
+	private static final String CICLE_FORMATIU = "1º DAM/DAW";
+	private static final String COGNOMS_ALUMNE = "Candela";
+	private static final String NOM_ALUMNE = "Arturo";
+
 	public static void main(String[] args) {
 		
 		Alumne persona = new Alumne();
 		
-		persona.nombre = "Arturo";
-		persona.cognoms = "Candela";
-		persona.cicleFormatiu = "1º DAM/DAW";
-		persona.any = 2021;
+		persona.nombre = NOM_ALUMNE;
+		persona.cognoms = COGNOMS_ALUMNE;
+		persona.cicleFormatiu = CICLE_FORMATIU;
+		persona.any = ANY;
 		
 		System.out.println(persona.toString());
 
