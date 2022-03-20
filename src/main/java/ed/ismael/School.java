645806b106fc25f73115c34308dd9ac61e0d25c3
ismaelmdv <ismaelmarcos64@gmail.com>
2022-03-20 21:59:05 +0100

    Refactorizar nombre paquete y objeto persona

---
+++ src/main/java/ed/ismael/School.java
@@ -0,0 +1,18 @@
+package ed.ismael;
+
+public class School {
+
+	public static void main(String[] args) {
+		
+		Persona persona = new Persona();
+		
+		persona.n = "Arturo";
+		persona.c = "Candela";
+		persona.cl = "1º DAM/DAW";
+		persona.a = 2021;
+		
+		System.out.println(persona.toString());
+
+	}
+
+}
