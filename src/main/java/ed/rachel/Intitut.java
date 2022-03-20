fa63ce31fa77a544a9a1d767a7c8512020467ce6
Rachel Sama <raquelsamagi@gmail.com>
2022-03-20 16:23:50 +0100

    [MOD] Refactor sacar constantes del metodo main()

--- src/main/java/ed/rachel/Intitut.java
+++ src/main/java/ed/rachel/Intitut.java
@@ -2,14 +2,19 @@ package ed.rachel;
 
 public class Intitut {
 
+	private static final int ANY = 2022;
+	private static final String CICLE_FORMATIU = "1º DAW";
+	private static final String COGNOMS_ALUMNE = "Samaniego";
+	private static final String NOM_ALUMNE = "Rachel";
+
 	public static void main(String[] args) {
 		
 		Alumne p = new Alumne();
 		
-		p.nom = "Rachel";
-		p.cognoms = "Samaniego";
-		p.cicleFormatiu = "1º DAW";
-		p.any = 2022;
+		p.nom = NOM_ALUMNE;
+		p.cognoms = COGNOMS_ALUMNE;
+		p.cicleFormatiu = CICLE_FORMATIU;
+		p.any = ANY;
 		
 		System.out.println(p.toString());
 
